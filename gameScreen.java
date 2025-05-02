7576359d9430826a8c1509f2e441ad6a8f7a3ef1
abdldrday <abdildirday14@gmail.com>
2025-05-02 23:15:41 +0500

    first element

--- gameScreen.java
+++ gameScreen.java
@@ -30,7 +30,23 @@ public class gameScreen extends JPanel implements Runnable {
     public void run() {
 
         while(gameThread != null){
-            
+
+            update();
+
+            repaint();
         }
+    }
+
+    public void update(){}
+
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(100, 100, titlSize, titlSize);
+        g2.dispose();
     }
 }
