0eb31b23734d92d31448ead46d8aa3d9f569993b
abdldrday <abdildirday14@gmail.com>
2025-05-02 22:23:19 +0500

    window settings

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("hello");;
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Invincible game");
+
+        gameScreen gameScreen = new gameScreen();
+        window.add(gameScreen);
+
+        window.pack(); 
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
--- gameScreen.java
+++ gameScreen.java
@@ -1,11 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class gameScreen extends JPanel {
+
+//    Screen settings
     final int originalTitle = 16;
     final int scale = 3;
 
     final int titlSize = originalTitle * scale;
     final int maxScreenCol = 16;
-    final int MaxScreenRow = 12;
+    final int maxScreenRow = 12;
+    final int screenWidth = titlSize * maxScreenCol;
+    final int screenHeight = titlSize * maxScreenRow;
 
+    public gameScreen(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
 }
