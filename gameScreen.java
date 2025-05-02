703370a07000963ab24ec24ba6dcdfad90d3bb69
abdldrday <abdildirday14@gmail.com>
2025-05-02 22:01:33 +0500

    first commit

---
+++ gameScreen.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class gameScreen extends JPanel {
+    final int originalTitle = 16;
+    final int scale = 3;
+
+    final int titlSize = originalTitle * scale;
+    final int maxScreenCol = 16;
+    final int MaxScreenRow = 12;
+
+}
